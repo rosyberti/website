4f59fa9e5ca4a29fd4bf22294b21bf28005b60f6
Nicholas McCullum <nicholasmccullum@gmail.com>
2020-03-23 21:03:24 -0300

    added code to create Markdown document

---
+++ code/coronavirus-data-by-country/blog-post-generator.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+data = pd.read_json('https://raw.githubusercontent.com/pomber/covid19/master/docs/timeseries.json')
+
+data = data.reindex(sorted(data.columns), axis=1)
+
+for country in data.columns:
+    print("""
+## """+country+"""
+
+!["""+country+""" Coronavirus Cases](https://coronavirus-country-charts.s3.ca-central-1.amazonaws.com/"""+country+"""+Cases.png)
+
+!["""+country+""" Coronavirus Deaths](https://coronavirus-country-charts.s3.ca-central-1.amazonaws.com/"""+country+"""+Deaths.png)
+
+!["""+country+""" Coronavirus Recoveries](https://coronavirus-country-charts.s3.ca-central-1.amazonaws.com/"""+country+"""+Recoveries.png)
+    """)
