927724c285f0f44c4d2e050815d38a6586fe3011
Nicholas McCullum <nicholasmccullum@gmail.com>
2020-03-24 17:25:12 -0300

    added all countries

--- code/coronavirus-scatterplot/coronavirus-scatterplot-generator.py
+++ code/coronavirus-scatterplot/coronavirus-scatterplot-generator.py
@@ -1,1 +1,13 @@
-#placeholder
+import numpy as np
+import pandas as pd
+
+countries = ['China', 
+             'India', 
+             'United States', 
+             'Indonesia', 
+             'Pakistan', 
+             'Brazil', 
+             'Nigeria', 
+             'Bangladesh', 
+             'Russia', 
+             'Mexico']
