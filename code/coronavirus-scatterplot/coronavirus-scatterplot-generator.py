7adf2cea5b25f8cb4ed44c1e735e73f677e0b01a
Nicholas McCullum <nicholasmccullum@gmail.com>
2020-03-25 21:16:44 -0300

    changed country to dictionary with ETF tickers

--- code/coronavirus-scatterplot/coronavirus-scatterplot-generator.py
+++ code/coronavirus-scatterplot/coronavirus-scatterplot-generator.py
@@ -2,16 +2,18 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-countries = ['China', 
-             'India', 
-             'United States', 
-             'Indonesia', 
-             'Pakistan', 
-             'Brazil', 
-             'Nigeria', 
-             'Bangladesh', 
-             'Russia', 
-             'Mexico']
+countryETFs = {
+              'United States': 'SPY',
+              'China': 'MCHI', 
+              'Japan': 'EWJ',
+              'Germany': 'EWG',
+              'India': 'INDA', 
+              'United Kingdom': 'EWU',
+              'France': 'EWQ',
+              'Italy': 'EWI',
+              'Brazil': 'EWZ',
+              'Canada': 'EWC'
+              }
 
 populationData = pd.read_json('https://raw.githubusercontent.com/samayo/country-json/master/src/country-by-population.json')
 
